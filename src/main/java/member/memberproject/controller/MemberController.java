68e9abe50c2da492cb11ea87503b20ae69a8fe10
YS <yooooonnf@gmail.com>
2022-01-11 22:17:32 +0900

    web mvc study commit

--- src/main/java/member/memberproject/controller/MemberController.java
+++ src/main/java/member/memberproject/controller/MemberController.java
@@ -1,8 +1,14 @@
 package member.memberproject.controller;
 
+import member.memberproject.domain.Member;
 import member.memberproject.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,9 +29,31 @@ public class MemberController {
 
 
     private final MemberService memberService;
+
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }//생성자주입
     //생성자 주입을 일반적으로 사용한다
+
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
---
+++ src/main/java/member/memberproject/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package member.memberproject.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
