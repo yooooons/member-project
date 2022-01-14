50f10fee9ef5053de5155b4edaa4ba5d2c4dff31
YS <yooooonnf@gmail.com>
2022-01-14 22:59:43 +0900

    jdbcTemplate stu

--- src/main/java/member/memberproject/SpringConfig.java
+++ src/main/java/member/memberproject/SpringConfig.java
@@ -1,6 +1,7 @@
 package member.memberproject;
 
 import member.memberproject.repository.JdbcMemberRepository;
+import member.memberproject.repository.JdbcTemplateMemberRepository;
 import member.memberproject.repository.MemberRepository;
 import member.memberproject.repository.MemoryMemberRepository;
 import member.memberproject.service.MemberService;
@@ -28,6 +29,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);//이렇게  @을 사용하면 메모리데이터에서 데이터베이스로 변경이 쉽다/다형성활용을 활용한 스프링사용 장점
+//        return new JdbcMemberRepository(dataSource);//이렇게  @을 사용하면 메모리데이터에서 데이터베이스로 변경이 쉽다/다형성활용을 활용한 스프링사용 장점
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
